168caf9bb96db356079e67c3f5135ebaa3739b51
gordanet <162464057+gordanet@users.noreply.github.com>
2024-05-27 23:58:48 +0300

    Add files via upload

---
+++ cmd/gorbridge/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"flag"
+	"io/ioutil"
+	"log"
+	"os"
+	"path"
+	"time"
+	"github.com/onemorebsmith/gorstratum/src/gorstratum"
+	"gopkg.in/yaml.v2"
+	"db"
+)
+
+func main() {
+	pwd, _ := os.Getwd()
+	fullPath := path.Join(pwd, "config.yaml")
+	log.Printf("loading config @ `%s`", fullPath)
+	rawCfg, err := ioutil.ReadFile(fullPath)
+	if err != nil {
+		log.Printf("config file not found: %s", err)
+		os.Exit(1)
+	}
+	cfg := gorstratum.BridgeConfig{}
+	if err := yaml.Unmarshal(rawCfg, &cfg); err != nil {
+		log.Printf("failed parsing config file: %s", err)
+		os.Exit(1)
+	}
+	flag.StringVar(&cfg.StratumPort, "stratum", cfg.StratumPort, "stratum port to listen on, default `:6969`")
+	flag.BoolVar(&cfg.PrintStats, "stats", cfg.PrintStats, "true to show periodic stats to console, default `true`")
+	flag.StringVar(&cfg.RPCServer, "gor", cfg.RPCServer, "address of the gord node, default `localhost:26110`")
+	flag.DurationVar(&cfg.BlockWaitTime, "blockwait", cfg.BlockWaitTime, "time in ms to wait before manually requesting new block, default `500`")
+	flag.UintVar(&cfg.MinShareDiff, "mindiff", cfg.MinShareDiff, "minimum share difficulty to accept from miner(s), default `4`")
+	flag.UintVar(&cfg.ExtranonceSize, "extranonce", cfg.ExtranonceSize, "size in bytes of extranonce, default `0`")
+	flag.StringVar(&cfg.PromPort, "prom", cfg.PromPort, "address to serve prom stats, default `:6119`")
+	flag.BoolVar(&cfg.UseLogFile, "log", cfg.UseLogFile, "if true will output errors to log file, default `true`")
+	flag.StringVar(&cfg.HealthCheckPort, "hcp", cfg.HealthCheckPort, `(rarely used) if defined will expose a health check on /readyz, default ""`)
+	flag.StringVar(&cfg.PoolAddress, "pa", cfg.PoolAddress, "pool address to serve, default `gor:qzqw1qvsvm113j1t39280bxqkr6fnph6c9r8ygrdgrqyd4czecggjpwoet9pk`")
+	flag.Parse()
+	if cfg.MinShareDiff == 0 {
+		cfg.MinShareDiff = 4
+	}
+	if cfg.BlockWaitTime == 0 {
+		cfg.BlockWaitTime = 5 * time.Second // this should never happen due to gor 1s block times
+	}
+	log.Println("----------------------------------")
+	log.Printf("initializing bridge")
+	log.Printf("\tgord:          %s", cfg.RPCServer)
+	log.Printf("\tstratum:         %s", cfg.StratumPort)
+	log.Printf("\tprom:            %s", cfg.PromPort)
+	log.Printf("\tstats:           %t", cfg.PrintStats)
+	log.Printf("\tlog:             %t", cfg.UseLogFile)
+	log.Printf("\tmin diff:        %d", cfg.MinShareDiff)
+	log.Printf("\tblock wait:      %s", cfg.BlockWaitTime)
+	log.Printf("\textranonce size: %d", cfg.ExtranonceSize)
+	log.Printf("\thealth check:    %s", cfg.HealthCheckPort)
+	log.Printf("\tpool address:    %s", cfg.PoolAddress)
+	log.Println("----------------------------------")
+    err = db.InitConnection()
+    if err != nil {
+        panic(err)
+    }
+    log.Println("Connected to the database!")
+	db.PA = cfg.PoolAddress
+	if err := gorstratum.ListenAndServe(cfg); err != nil {
+		log.Println(err)
+	}
+}
